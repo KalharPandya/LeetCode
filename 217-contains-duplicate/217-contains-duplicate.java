4c881151fa3b2d6e19db5f67b13b19c1ba2028a0
Kalhar Pandya <66383494+KalharPandya@users.noreply.github.com>
2022-01-07 00:14:17 +0530

    Time: 13 ms (25.54%), Space: 74.1 MB (5.74%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,43 +1,12 @@
 class Solution { 
     
-    public static void merge(int []arr, int l,int m, int r){
-        int k = l;
-        int n1 = m - l + 1;
-        int n2 = r-m;
-        int [] larr = new int[n1];
-        int [] rarr = new int[n2];
-        for (int i = 0; i < n1; i++) larr[i] = arr[l + i];
-        for (int i = 0; i < n2; i++) rarr[i] = arr[m + i +1];
-        int i = 0;
-        int j = 0;
-        while (i < n1 && j < n2){
-            if(larr[i] < rarr[j])
-                arr[k++] = larr[i++];
-            else
-                arr[k++] = rarr[j++];
-        }
-        while (i<n1)    arr[k++] = larr[i++];
-        while (j<n2)    arr[k++] = rarr[j++];
-    }
-
-    public static void sort(int [] arr, int l, int r){
-        if(l<r){
-            int mid = (l+r)/2;
-            sort(arr,l,mid);
-            sort(arr,mid+1,r);
-            merge(arr,l,mid,r);
-        }
-    }
-    public static void sort(int [] arr){
-        sort(arr,0,arr.length-1);
-    }
-    public boolean containsDuplicate(int[] nums) {
-        sort(nums);
-        int prev = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            if(prev == nums[i])
+        public HashMap<Integer, Boolean> map = new HashMap<>();
+        public boolean containsDuplicate(int[] nums) {
+        for (int i:
+             nums) {
+            if(map.containsKey(i))
                 return true;
-            prev = nums[i];
+            map.put(i,true);
         }
         return false;
     }
