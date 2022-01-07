6a3ed67862a3fe3ae6c3d0a2f21f9bdfb2487583
Kalhar Pandya <66383494+KalharPandya@users.noreply.github.com>
2022-01-07 22:38:42 +0530

    Time: 14 ms (24.77%), Space: 38.9 MB (84.49%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -0,0 +1,26 @@
+class Solution {
+        public boolean boxes[][] = new boolean[9][9];
+    public boolean cols[][] = new boolean[9][9];
+    public boolean rows[][] = new boolean[9][9];
+    int n = 0;
+    public boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if(board[i][j] =='.')
+                    continue;
+                System.out.println(Character.getNumericValue(board[i][j]));
+                n = Character.getNumericValue(board[i][j]) - 1;
+                if(rows[i][n])
+                    return false;
+                if(cols[j][n])
+                    return false;
+                if(boxes[(i/3)*3 + j/3][n])
+                    return false;
+                rows[i][n] = true;
+                cols[j][n] = true;
+                boxes[(i/3)*3 + j/3][n] = true;
+            }
+        }
+        return true;
+    }
+}
