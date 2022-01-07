0bafade9a270290dbaad0c79abc69056882853fb
Kalhar Pandya <66383494+KalharPandya@users.noreply.github.com>
2022-01-07 17:23:09 +0530

    Time: 1 ms (98.42%), Space: 40.7 MB (15.81%) - LeetHub

---
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        int []db = new int[1001];
+        int ans[] = new int[Math.min(nums1.length, nums2.length)];
+        int aindex = 0;
+        for (int i:
+             nums1) {
+            db[i]+=1;
+        }
+        for (int i:
+                nums2) {
+            if(db[i]-->=1){
+                ans[aindex++] = i;
+            }
+        }
+        int[] fans = new int[aindex];
+        for (int i = 0; i < aindex; i++) {
+            fans[i] = ans[i];
+        }
+        return fans;
+    }
+}
