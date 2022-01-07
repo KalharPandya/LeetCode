4857c61ad8587f6a14913c527a726e82a57463dd
Kalhar Pandya <66383494+KalharPandya@users.noreply.github.com>
2022-01-07 23:47:15 +0530

    Time: 28 ms (27.87%), Space: 45.4 MB (24.18%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,13 +1,27 @@
+import java.util.*;
 class Solution {
     public int firstUniqChar(String s) {
         int db[] = new int[26];
-        char funique = s.charAt(0);
+        int funique = -1;
+        Deque<Integer> q
+                = new ArrayDeque<>();
+        q.addFirst(-1);
         for (int i = s.length()-1; i >=0 ; i--) {
-            db[s.charAt(i) - 'a'] ++;
+            db[s.charAt(i) - 'a']++;
+            if (q.peekFirst() != -1 && db[s.charAt(q.peekFirst()) - 'a'] > 1)
+                q.removeFirst();
+            if (db[s.charAt(i) - 'a'] == 1) {
+                q.addFirst(i);
+                funique = i;
+            }
         }
-        for (int i = 0; i < s.length(); i++) {
-            if(db[s.charAt(i) - 'a'] == 1){
-                return i;
+        int n = 0;
+        while (!q.isEmpty()){
+            n=q.pollFirst();
+            if(n==-1)
+                return n;
+            if(db[s.charAt(n)-'a']==1){
+                return n;
             }
         }
         return -1;
