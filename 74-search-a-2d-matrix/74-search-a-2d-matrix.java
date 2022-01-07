cec30cba6859e6a4b1be7c40998430e50a5f2411
Kalhar Pandya <66383494+KalharPandya@users.noreply.github.com>
2022-01-07 23:14:43 +0530

    Time: 1 ms (15.68%), Space: 39.3 MB (22.51%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,34 +1,27 @@
 class Solution {
-    public int m = 0;
-    public int findRow(int[][]arr,int l, int r, int x) {
-        if (r < l)
-            return -1;
-        m = (l + r) / 2;
-        if (arr[m][0] <= x && arr[m][arr[m].length - 1] >= x) {
-            return m;
-        }
-        if (arr[m][0] > x)
-            return findRow(arr, l, m - 1, x);
-
-        return findRow(arr, m + 1, r, x);
-    }
-    public boolean findCol(int[][]arr,int row,int l, int r, int x) {
-        if(row<0)
-            return false;
-        if (r < l)
-            return false;
-        m = (l + r) / 2;
-        if (arr[row][m] == x) {
-            return true;
-        }
-        if (arr[row][m] > x)
-            return findCol(arr,row, l, m - 1, x);
-
-        return findCol(arr,row,m + 1, r, x);
-    }
-
-
     public boolean searchMatrix(int[][] matrix, int target) {
-        return findCol(matrix,(findRow(matrix,0,matrix.length-1,target)),0,matrix[0].length-1,target);
+        
+        int r=matrix.length;
+        int c=matrix[0].length;
+        int row=0;
+        for(int i=0;i<r;i++)
+        {
+            if(target>matrix[i][c-1])
+                continue;
+            else if(target==matrix[i][c-1])
+                return true;
+            else {
+                row = i;
+                break;
+            }
+        }
+        if(target<matrix[row][0])
+            return false;
+        for(int j=0;j<c;j++)
+        {
+            if(matrix[row][j]==target)
+                return true;
+        }
+        return false;
     }
 }
