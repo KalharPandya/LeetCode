3d8cbafdaf9effc5a9f2804e8ea96c93c9f7b4ce
Kalhar Pandya <66383494+KalharPandya@users.noreply.github.com>
2022-01-18 22:57:46 +0530

    Time: 5 ms (15.14%), Space: 53.2 MB (21.60%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] sortedSquares(int[] nums) {
         
         for(int i = 0 ; i < nums.length ; i ++){
-            nums[i] = nums[i]*nums[i];
+            nums[i] = (int)Math.pow(nums[i],2);
         }
         Arrays.sort(nums);
         return nums;   
