6560dbe66fe4b512bc81f5d4a733ed6a8b3077b1
Kalhar Pandya <66383494+KalharPandya@users.noreply.github.com>
2022-01-19 23:35:04 +0530

    Time: 3 ms (11.00%), Space: 52.4 MB (33.25%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -3,9 +3,9 @@ class Solution {
         int length = s.length;
         for(int i = 0 ; i < length/2 ; i ++){
             
-            s[i] += (char)s[length - 1 - i];
+            s[i] += s[length - 1 - i];
             s[length - 1 - i] = (char)(s[i] - s[length - 1 - i]);
-            s[i]-=(char)s[length - 1 - i];
+            s[i]-=s[length - 1 - i];
         }
     }
 }
