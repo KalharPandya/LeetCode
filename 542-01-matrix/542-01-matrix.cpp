e98826455f869adf859754876da9e59d44f63212
Kalhar Pandya <66383494+KalharPandya@users.noreply.github.com>
2022-01-23 01:40:08 +0530

    Time: 128 ms (30.91%), Space: 27.6 MB (79.50%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
-        int rows = matrix.size();
-        if (rows == 0) 
-            return matrix;
-        int cols = matrix[0].size();
-        vector<vector<int>> dist(rows, vector<int> (cols, INT_MAX - 100000));
-
-        //First pass: check for left and top
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (matrix[i][j] == 0) {
-                    dist[i][j] = 0;
-                } else {
-                    if (i > 0)
-                        dist[i][j] = min(dist[i][j], dist[i - 1][j] + 1);
-                    if (j > 0)
-                        dist[i][j] = min(dist[i][j], dist[i][j - 1] + 1);
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat){
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        int m=mat.size(),n=mat[0].size();
+        
+        vector<vector<int>> ans(m,vector<int>(n,999999));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0)
+                    ans[i][j]=0;
+                else{
+                    if(i>0)
+                        ans[i][j]=min(ans[i][j],ans[i-1][j]+1);
+                    if(j>0)
+                        ans[i][j]=min(ans[i][j],ans[i][j-1]+1);
                 }
             }
         }
-
-        //Second pass: check for bottom and right
-        for (int i = rows - 1; i >= 0; i--) {
-            for (int j = cols - 1; j >= 0; j--) {
-                if (i < rows - 1)
-                    dist[i][j] = min(dist[i][j], dist[i + 1][j] + 1);
-                if (j < cols - 1)
-                    dist[i][j] = min(dist[i][j], dist[i][j + 1] + 1);
+        
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(i<m-1)
+                    ans[i][j]=min(ans[i][j],ans[i+1][j]+1);
+                if(j<n-1)
+                    ans[i][j]=min(ans[i][j],ans[i][j+1]+1);
             }
         }
-        return dist;
+        
+        return ans;
     }
 };
